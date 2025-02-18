cb8e078580500706eb96671cfb4703fd556c66bf
Jumilo98 <jgranizo98@hotmail.com>
2025-02-18 01:31:42 -0500

    Commit 8: Correcion

--- src/app/components/MovieCard.tsx
+++ src/app/components/MovieCard.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
+import IconHeart from './IconHeart';
+import IconStar from './IconStar';
 
 interface Movie {
   id: string;
@@ -7,6 +9,10 @@ interface Movie {
   primaryImage: string;
   description: string;
   releaseDate: string;
+  numVotes: number;
+  genres: string;
+  contentRating: string;
+  averageRating: number;
 }
 
 interface MovieCardProps {
@@ -16,14 +22,42 @@ interface MovieCardProps {
 const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
   return (
     <Link href={`/movie/${movie.id}`}>
-      <div className="max-w-[22rem] w-full bg-white shadow-[4px_6px_10px_-3px_#bfc9d4] rounded border border-[#e0e6ed] dark:border-[#1b2e4b] dark:bg-[#191e3a] dark:shadow-none overflow-hidden">
+      <div className="max-w-[22rem] w-full bg-white shadow-[4px_6px_10px_-3px_#bfc9d4] rounded border border-white-light dark:border-[#1b2e4b] dark:bg-[#191e3a] dark:shadow-none overflow-hidden">
         <div className="py-7 px-6">
+          {/* Imagen */}
           <div className="-mt-7 mb-7 -mx-6 rounded-tl rounded-tr h-[260px] overflow-hidden">
             <img src={movie.primaryImage} alt={movie.primaryTitle} className="w-full h-full object-cover" />
           </div>
+
+          {/* Fecha y Título */}
           <p className="text-primary text-xs mb-1.5 font-bold">{movie.releaseDate}</p>
           <h5 className="text-[#3b3f5c] text-[15px] font-bold mb-4 dark:text-white-light">{movie.primaryTitle}</h5>
           <p className="text-white-dark">{movie.description}</p>
+
+          {/* Sección de Autor y Métricas */}
+          <div className="relative flex justify-between mt-6 pt-4 before:w-[250px] before:h-[1px] before:bg-white-light before:inset-x-0 before:top-0 before:absolute before:mx-auto dark:before:bg-[#1b2e4b]">
+            {/* Autor */}
+            <div className="flex items-center font-semibold">
+              <div className="w-9 h-9 rounded-full overflow-hidden inline-block mr-2">
+                <span className="flex w-full h-full items-center justify-center bg-[#515365] text-white-light">
+                  {movie.contentRating}
+                </span>
+              </div>
+              <div className="text-[#515365] dark:text-white-dark">{movie.genres[0]}</div>
+            </div>
+
+            {/* Likes y Vistas */}
+            <div className="flex font-semibold">
+              <div className="text-primary flex items-center mr-3">
+                <IconHeart className="w-4 h-4 mr-1" />
+                {movie.numVotes}
+              </div>
+              <div className="text-primary flex items-center">
+                <IconStar className="w-4 h-4 mr-1" />
+                {movie.averageRating}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </Link>
--- src/app/components/Pagination.tsx
+++ src/app/components/Pagination.tsx
@@ -1,3 +1,5 @@
+import IconCaretDown from "./IconCaretDown";
+import IconCaretsDown from "./IconCaretsDown";
 import React from "react";
 
 interface PaginationProps {
@@ -10,56 +12,84 @@ const Pagination: React.FC<PaginationProps> = ({ page, totalPages, onPageChange 
   const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
 
   return (
-    <ul className="inline-flex items-center m-auto mt-6">
-      {/* Botón Anterior */}
-      <li>
-        <button
-          type="button"
-          className={`flex justify-center font-semibold ltr:rounded-l-full rtl:rounded-r-full px-3.5 py-2 transition ${
-            page === 1
-              ? "bg-gray-300 cursor-not-allowed text-gray-500"
-              : "bg-white-light text-dark hover:text-white hover:bg-primary dark:text-white-light dark:bg-[#191e3a] dark:hover:bg-primary"
-          }`}
-          onClick={() => onPageChange(page - 1)}
-          disabled={page === 1}
-        >
-          ⬅️
-        </button>
-      </li>
+    <div className="mb-5">
+      <div className="flex justify-center flex-col w-full">
+        <ul className="inline-flex items-center m-auto mb-4">
+          {/* Botón "Primera Página" */}
+          <li>
+            <button
+              type="button"
+              className={`flex justify-center font-semibold rounded-l-full px-3.5 py-2 transition ${
+                page === 1 ? "bg-gray-300 cursor-not-allowed text-gray-500" : "bg-white-light text-dark hover:text-white hover:bg-primary dark:text-white-light dark:bg-[#191e3a] dark:hover:bg-primary"
+              }`}
+              onClick={() => onPageChange(1)}
+              disabled={page === 1}
+            >
+              <IconCaretsDown className="rotate-90" />
+            </button>
+          </li>
 
-      {/* Números de Página */}
-      {pages.map((pageNumber) => (
-        <li key={pageNumber}>
-          <button
-            type="button"
-            className={`flex justify-center font-semibold px-3.5 py-2 transition ${
-              page === pageNumber
-                ? "bg-primary text-white dark:text-white-light dark:bg-primary"
-                : "bg-white-light text-dark hover:text-white hover:bg-primary dark:text-white-light dark:bg-[#191e3a] dark:hover:bg-primary"
-            }`}
-            onClick={() => onPageChange(pageNumber)}
-          >
-            {pageNumber}
-          </button>
-        </li>
-      ))}
+          {/* Botón "Anterior" */}
+          <li>
+            <button
+              type="button"
+              className={`flex justify-center font-semibold px-3.5 py-2 transition ${
+                page === 1 ? "bg-gray-300 cursor-not-allowed text-gray-500" : "bg-white-light text-dark hover:text-white hover:bg-primary dark:text-white-light dark:bg-[#191e3a] dark:hover:bg-primary"
+              }`}
+              onClick={() => onPageChange(page - 1)}
+              disabled={page === 1}
+            >
+              <IconCaretDown className="w-5 h-5 rotate-90" />
+            </button>
+          </li>
 
-      {/* Botón Siguiente */}
-      <li>
-        <button
-          type="button"
-          className={`flex justify-center font-semibold ltr:rounded-r-full rtl:rounded-l-full px-3.5 py-2 transition ${
-            page === totalPages
-              ? "bg-gray-300 cursor-not-allowed text-gray-500"
-              : "bg-white-light text-dark hover:text-white hover:bg-primary dark:text-white-light dark:bg-[#191e3a] dark:hover:bg-primary"
-          }`}
-          onClick={() => onPageChange(page + 1)}
-          disabled={page === totalPages}
-        >
-          ➡️
-        </button>
-      </li>
-    </ul>
+          {/* Números de Página */}
+          {pages.map((pageNumber) => (
+            <li key={pageNumber}>
+              <button
+                type="button"
+                className={`flex justify-center font-semibold px-3.5 py-2 transition ${
+                  page === pageNumber
+                    ? "bg-primary text-white dark:text-white-light dark:bg-primary"
+                    : "bg-white-light text-dark hover:text-white hover:bg-primary dark:text-white-light dark:bg-[#191e3a] dark:hover:bg-primary"
+                }`}
+                onClick={() => onPageChange(pageNumber)}
+              >
+                {pageNumber}
+              </button>
+            </li>
+          ))}
+
+          {/* Botón "Siguiente" */}
+          <li>
+            <button
+              type="button"
+              className={`flex justify-center font-semibold px-3.5 py-2 transition ${
+                page === totalPages ? "bg-gray-300 cursor-not-allowed text-gray-500" : "bg-white-light text-dark hover:text-white hover:bg-primary dark:text-white-light dark:bg-[#191e3a] dark:hover:bg-primary"
+              }`}
+              onClick={() => onPageChange(page + 1)}
+              disabled={page === totalPages}
+            >
+              <IconCaretDown className="w-5 h-5 -rotate-90" />
+            </button>
+          </li>
+
+          {/* Botón "Última Página" */}
+          <li>
+            <button
+              type="button"
+              className={`flex justify-center font-semibold rounded-r-full px-3.5 py-2 transition ${
+                page === totalPages ? "bg-gray-300 cursor-not-allowed text-gray-500" : "bg-white-light text-dark hover:text-white hover:bg-primary dark:text-white-light dark:bg-[#191e3a] dark:hover:bg-primary"
+              }`}
+              onClick={() => onPageChange(totalPages)}
+              disabled={page === totalPages}
+            >
+              <IconCaretsDown className="-rotate-90" />
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
